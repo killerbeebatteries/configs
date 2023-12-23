7367785a32120ae65d4eef732751673177f4cdb5
killerbeebatteries <somewhere@example.com>
2023-12-23 10:51:23 +0930

    Current nvim config

---
+++ nvim/nvchad/lua/custom/mappings.lua
@@ -0,0 +1,27 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+      }
+  }
+}
+
+return M
